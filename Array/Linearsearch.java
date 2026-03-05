19964fdeced18181ebb7d2fc2096a6ead0709c93
Anwesha Barad <anweshabarad8@gmail.com>
2026-03-05 21:44:07 +0530

    Added Linear Search problem

---
+++ Array/Linearsearch.java
@@ -0,0 +1,29 @@
+//Linear search
+
+import java.util.Scanner;
+class Linearsearch{
+    public static void main(String s[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter index size: ");
+        int n=sc.nextInt();
+         System.out.print("Enter element: ");
+        int arr[]=new int[5];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter element for search: ");
+       int key=sc.nextInt();
+       boolean found=false;
+       for(int i=0;i<n;i++){
+        if(arr[i]==key){
+             System.out.print("element found at index: "+i);
+             found=true;
+             break;
+        }
+       }
+       if(!found){
+             System.out.print("element not found ");
+        }
+    }
+}
