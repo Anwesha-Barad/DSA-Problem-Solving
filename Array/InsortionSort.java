d1592a0315b620b1d59be81174fe9db3fdd1ac51
Anwesha Barad <anweshabarad8@gmail.com>
2026-03-13 19:17:27 +0530

    add Insortion Sort

---
+++ Array/InsortionSort.java
@@ -0,0 +1,31 @@
+//insertion sort
+
+import java.util.Scanner;
+class InsortionSort{
+    public static void main(String s[]){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter the index size:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+         System.out.println("enter the element for array:");
+         for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+         }
+         for(int i=1;i<n;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+         }
+         System.out.println("sorted array:" );
+         for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+         }
+         
+        }
+    }
