a414543b54e3f804a79df77c83a40efc34a9fdfe
Anwesha Barad <anweshabarad8@gmail.com>
2026-03-22 16:50:08 +0530

    Cleaned files and updated code

--- Array/Linearsearch.java
+++ Array/Linearsearch.java
@@ -7,7 +7,7 @@ class Linearsearch{
         System.out.print("Enter index size: ");
         int n=sc.nextInt();
          System.out.print("Enter element: ");
-        int arr[]=new int[5];
+        int arr[]=new int[n];
 
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
