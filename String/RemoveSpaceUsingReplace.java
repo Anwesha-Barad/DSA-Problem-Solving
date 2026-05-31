0dd505e4fd1e6e8e42350e3ef64e3843bd52a20d
Anwesha Barad <anweshabarad8@gmail.com>
2026-05-31 23:43:13 +0530

    Renamed string programs

--- String/RemoveSpaceUsingReplace.java
+++ String/RemoveSpaceUsingReplace.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-class RemoveVowels{
+class RemoveSpaceUsingReplace{
     public static void main(String s[]){
       
         Scanner sc=new Scanner(System.in);
          System.out.println("Enter String with space:");
         String str=sc.nextLine();
-        str= str.replaceAll("[AEIOUaeiou]","");
+        str= str.replace(" ","");
         System.out.println("after removing space the string is:"+str);
     }
 }
--- String/RemoveVowelsUsingReplace.java
+++ String/RemoveVowelsUsingReplace.java
@@ -1,11 +1,11 @@
-import java.util.*;
-class SpaceRemove{
+import java.util.Scanner;
+class RemoveVowelsUsingReplace{
     public static void main(String s[]){
       
         Scanner sc=new Scanner(System.in);
          System.out.println("Enter String with space:");
         String str=sc.nextLine();
-        str= str.replace(" ","");
+        str= str.replaceAll("[AEIOUaeiou]","");
         System.out.println("after removing space the string is:"+str);
     }
 }
