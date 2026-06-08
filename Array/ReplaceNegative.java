60aee1e5964bd9cee35758d1a28f8ce8cd43e315
Anwesha Barad <anweshabarad8@gmail.com>
2026-06-08 23:16:20 +0530

    Added ReplaceNegative program

---
+++ Array/ReplaceNegative.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class ReplaceNegative{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of the array:");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] < 0) {
+                arr[i] = 0;
+            }
+        }
+
+        System.out.println("Output:");
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
